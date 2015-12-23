db48d6ed510c52355ae024741dd8b4f4f7a9c6d8
valeriy.tereshenko <tereshenko.valeriy.v@gmail.com>
2015-12-23 10:49:56 +0200

    Draggable/element/MoveController

--- ui/i/draggable/element/directive.js
+++ ui/i/draggable/element/directive.js
@@ -1,6 +1,26 @@
 /**
  * Created on 22.12.2015.
  */
-angular.module('draggable').directive('draggableElement', [function () {
-    return {};
+angular.module('draggable').directive('draggableElement', ['$document', 'draggableElementFactory', function ($document, draggableElementFactory) {
+    return {
+        restrict: 'E',
+        transclude: true,
+        templateUrl: 'ui/i/draggable/element/v.html',
+
+        link: function (scope, element, attrs) {
+            var moveController = new draggableElementFactory.MoveController({
+                onStartMove: function (e, params) {
+                    console.log('moved: ', params);
+                },
+                onMoving: function (e, params) {
+                    console.log('moving: ', params);
+                },
+                onDrop: function () {
+                    console.log('dropped');
+                }
+            });
+
+            element.on('mousedown', moveController.onMouseDown);
+        }
+    };
 }]);
---
+++ ui/i/draggable/element/service.js
@@ -0,0 +1,76 @@
+/**
+ * Created on 23.12.2015.
+ */
+angular.module('draggable').factory('draggableElementFactory', ['$document', function ($document) {
+    var draggableElementFactory = {},
+        fStub = function () {
+        };
+
+    function makeStubbing(obj, keys) {
+        if (!obj)
+            throw new Error('Must pass obj to stub');
+
+        angular.forEach(keys, function (key) {
+            if (!obj[key])
+                obj[key] = fStub;
+        });
+    }
+
+
+    draggableElementFactory.MoveController = function (callbacks) {
+        var
+            startX = 0, startY = 0, x = 0, y = 0, state = 'inactive';
+
+        makeStubbing(callbacks, ['onStartMove', 'onMoving', 'onDrop']);
+
+
+        function isMoving(event, params) {
+            if (state == 'moving')
+                return true;
+
+            if (params.length < 5)
+                return false;
+
+            state = 'moving';
+            callbacks.onStartMove(event, params);
+            return false;
+        }
+
+
+        function mousemove(event) {
+            y = event.pageY - startY;
+            x = event.pageX - startX;
+
+            var params = {
+                length: Math.sqrt(x * x + y * y),
+                y: y,
+                x: x
+            };
+
+            if (isMoving(event, params))
+                callbacks.onMoving(event, params);
+        }
+
+        function mouseup() {
+            $document.off('mousemove', mousemove);
+            $document.off('mouseup', mouseup);
+            state = 'inactive';
+            callbacks.onDrop();
+        }
+
+
+        return {
+            onMouseDown: function () {
+                event.preventDefault();
+                startX = event.pageX - x;
+                startY = event.pageY - y;
+                $document.on('mousemove', mousemove);
+                $document.on('mouseup', mouseup);
+                state = 'inactive';
+            }
+        };
+    };
+
+
+    return draggableElementFactory;
+}]);
